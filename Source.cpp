61ea9df0f31b40747c4c226769a311b9ee14045d
HoangBao6226 <148759082+HoangBao6226@users.noreply.github.com>
2023-10-30 16:35:25 +0700

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -8,12 +8,5 @@ int hieu(int a, int b);
 int tich(int a, int b);
 void main()
 {
-int a, b;
-cout<<"Nhap a = ";
-cin>>a;
-cout<<"Nhap b = ";
-cin>>b;
-cout<<"Tong = " <<tong(a,b)<<endl;
-cout<<"Hieu = " <<hieu(a,b)<<endl;
-cout<<"Tich = " <<tich(a,b)<<endl;
+
 }
